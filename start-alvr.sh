8f58045dfdd26c05503486190352d1c00ba50c08
PLYSHKA <leruop@gmail.com>
2023-04-23 19:53:43 +0500

    Fixed issue with no dashboard in steamvr

--- start-alvr.sh
+++ start-alvr.sh
@@ -8,5 +8,7 @@ if [[ -z $WAYLAND_DISPLAY ]]; then
     xhost "+si:localuser:$USER" || (echor "Couldn't use xhost, please install it" && exit 1)
 fi
 
-distrobox-enter --name arch-alvr -- steam &
-distrobox-enter --name arch-alvr -- bash -c './start-vr.sh'
+echog "Starting up Steam"
+distrobox-enter --name arch-alvr --additional-flags "--env LANG=en_US.UTF-8 --env LC_ALL=en_US.UTF-8" -- steam &>/dev/null &
+echog "Starting up ALVR"
+distrobox-enter --name arch-alvr --additional-flags "--env LANG=en_US.UTF-8 --env LC_ALL=en_US.UTF-8" -- ./start-vr.sh
