8a42aeca58a1b8d57fa4d6591ebc366a4cec0a73
PLYSHKA <leruop@gmail.com>
2023-05-02 17:09:59 +0500

    Logging support from setup, various reformats, more messages.

--- setup-phase-4.sh
+++ setup-phase-4.sh
@@ -9,7 +9,7 @@ if [[ -z $prefix ]]; then
 fi
 
 echor "Phase 4"
-STEP_INDEX=1
+export STEP_INDEX=1
 cd "$prefix" || echor "Couldn't go into installation folder, aborting."
 
 # Get current gpu (and version in case if it's nvidia from configuration)
@@ -20,10 +20,10 @@ fi
 AUDIO_SYSTEM="$(head <specs.conf -2 | tail -1)"
 
 echog "Installing packages for base functionality."
-sudo pacman -Syu git vim base-devel noto-fonts xdg-user-dirs fuse libx264 sdl2 libva-utils --noconfirm || exit 1
+sudo pacman -q --noprogressbar -Syu git vim base-devel noto-fonts xdg-user-dirs fuse libx264 sdl2 libva-utils --noconfirm || exit 1
 echog "Installing steam, audio and driver packages."
 if [[ "$GPU" == "amd" ]]; then
-   sudo pacman -Syu libva-mesa-driver vulkan-radeon lib32-vulkan-radeon lib32-libva-mesa-driver --noconfirm || exit 1
+   sudo pacman -q --noprogressbar -Syu libva-mesa-driver vulkan-radeon lib32-vulkan-radeon lib32-libva-mesa-driver --noconfirm || exit 1
 elif [[ "$GPU" == "nvidia" ]]; then
    echog "Using host system driver mounts, not installing anything inside for nvidia drivers."
 else
@@ -31,16 +31,16 @@ else
    exit 1
 fi
 if [[ "$AUDIO_SYSTEM" == "pipewire" ]]; then
-   sudo pacman -Syu lib32-pipewire pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber --noconfirm || exit 1
+   sudo pacman -q --noprogressbar -Syu lib32-pipewire pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber --noconfirm || exit 1
 elif [[ "$AUDIO_SYSTEM" == "pulseaudio" ]]; then
-   sudo pacman -Syu pulseaudio pusleaudio-alsa --noconfirm || exit 1
+   sudo pacman -q --noprogressbar -Syu pulseaudio pusleaudio-alsa --noconfirm || exit 1
 else
    echor "Couldn't determine audio system: $AUDIO_SYSTEM, you may have issues with audio!"
 fi
 
-sudo pacman -Syu steam --noconfirm
+sudo pacman -q --noprogressbar -Syu steam --noconfirm
 
-STEP_INDEX=2
+export STEP_INDEX=2
 sleep 2
 
 # Ask user for installing steamvr
@@ -49,10 +49,12 @@ steam &>/dev/null &
 echog "After installing SteamVR, copy (ctrl + shift + c from terminal) and launch command bellow from your host terminal shell (outside this container) and press enter to continue there. This prevents annoying popup (yes/no with asking for superuser) that prevents steamvr from launching automatically."
 echog "sudo setcap CAP_SYS_NICE+ep $HOME/.steam/steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher"
 read
-echog "Now launch SteamVR once, close it and press enter here to continue again."
+echog "Now launch SteamVR once and close it."
+echog "At this point you can safely add your external library from the host system ('/home/$USER' is same from inside the script container as from outside)"
+echog "When ready for next step, press enter to continue."
 read
 
-STEP_INDEX=3
+export STEP_INDEX=3
 sleep 2
 
 # installing alvr
@@ -77,25 +79,24 @@ read
 echog "Downloading ALVR apk, you can install it now from the installation folder into your headset using either ADB or Sidequest on host."
 wget -q --show-progress "$ALVR_APK_LINK"
 echog "From this point on, alvr will automatically start with SteamVR. But it's still quite broken mechanism so we need to use additional script for auto-restart to work."
-echog "Don't close ALVR yet."
+echog "Don't close ALVR."
 
 STEP_INDEX=4
 sleep 2
 
 # installing wlxoverlay
-echog "Since SteamVR overlay is partially broken on Linux, we will use WlxOverlay, which works with both X11 and Wayland and has ability to control whole desktop from inside VR."
+echog "SteamVR overlay is partially broken on Linux and for replacement we will use WlxOverlay, which works with both X11 and Wayland and has ability to control whole desktop from inside VR."
 wget -q --show-progress "$WLXOVERLAY_LINK"
 chmod +x "$WLXOVERLAY_FILENAME"
 if [[ "$WAYLAND_DISPLAY" != "" ]]; then
-   echog "If you're on wayland (and not on wlroots-based compositor), it will ask for display to choose. Choose each displays sequentially if you have more than 1."
-else
-   echog "If you're using Xorg, you don't need to do anything'"
+   echog "If you're not (on wlroots-based compositor like Sway), it will ask for display to choose. Choose displays if you have more than 1."
 fi
 ./"$WLXOVERLAY_FILENAME" &>/dev/null &
 if [[ "$WAYLAND_DISPLAY" != "" ]]; then
-   echog "If everything went well, you might see little icon on your desktop that indicates that screenshare is happening (by WlxOverlay)"
+   echog "If everything went well, you might see little icon on your desktop that indicates that screenshare is happening (by WlxOverlay) created by xdg portal."
 fi
-echog "WlxOverlay adds itself to auto-startup so you don't need to do anything with it to make it autostart. Press enter to continue."
+echog "WlxOverlay adds itself to auto-startup so you don't need to do anything with it to make it autostart."
+echog "Press enter to continue."
 read
 
 STEP_INDEX=5
@@ -114,6 +115,9 @@ cd ..
 
 STEP_INDEX=6
 sleep 2
+
+# Added color to pacman for user convenience later
+echo "Color" | sudo tee -a /etc/pacman.conf
 
 # post messages
 echog "From that point on, ALVR should be installed and WlxOverlay should be working. Please refer to https://github.com/galister/WlxOverlay/wiki/Getting-Started to familiarise with controls."
--- uninstall.sh
+++ uninstall.sh
@@ -14,7 +14,9 @@ if [[ -z $ROOT_PERMS_COMMAND ]]; then
    ROOT_PERMS_COMMAND="sudo"
 fi
 
-podman stop $container_name
-DBX_SUDO_PROGRAM=$ROOT_PERMS_COMMAND distrobox-rm --rm-home $container_name
+podman stop $container_name 2>/dev/null
 
 $ROOT_PERMS_COMMAND rm -rf $prefix
+DBX_SUDO_PROGRAM=$ROOT_PERMS_COMMAND distrobox-rm --rm-home $container_name 2>/dev/null
+
+echog "Uninstall completed."
