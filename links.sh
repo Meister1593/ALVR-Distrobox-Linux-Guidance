a84d0c1f80bdb905ac401cdf3ca60e2e697e2d0a
PLYSHKA <leruop@gmail.com>
2023-05-14 15:16:24 +0500

    Updated alvr

--- links.sh
+++ links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ALVR_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-dev12%2Bnightly.2023.05.07/ALVR-x86_64.AppImage'
+ALVR_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-dev12%2Bnightly.2023.05.11/ALVR-x86_64.AppImage'
 ALVR_FILENAME='ALVR-x86_64.AppImage'
-ALVR_APK_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-dev12%2Bnightly.2023.05.07/alvr_client_android.apk'
+ALVR_APK_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-dev12%2Bnightly.2023.05.11/alvr_client_android.apk'
 WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/download/v1.2.6/WlxOverlay-v1.2.6-x86_64.AppImage'
 WLXOVERLAY_FILENAME='WlxOverlay-v1.2.6-x86_64.AppImage'
