74223b65e5f0bb784553ba36734e47fc941c5621
Meister1593 <leruop@Gmail.com>
2023-05-26 17:00:03 +0500

    Update links.sh
    Updated wlxoverlay to new release as rc doesn't exist now

--- links.sh
+++ links.sh
@@ -4,5 +4,5 @@ ALVR_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-de
 ALVR_FILENAME='ALVR-x86_64.AppImage'
 ALVR_APK_LINK='https://github.com/alvr-org/ALVR-nightly/releases/download/v20.0.0-dev13%2Bnightly.2023.05.21/alvr_client_android.apk'
 ALVR_APK_NAME='alvr_client_android.apk'
-WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/download/v1.2.7-rc3/WlxOverlay-v1.2.7-rc3-x86_64.AppImage'
+WLXOVERLAY_LINK='https://github.com/galister/WlxOverlay/releases/download/v1.3.0/WlxOverlay-v1.3.0-x86_64.AppImage'
 WLXOVERLAY_FILENAME='WlxOverlay.AppImage'
